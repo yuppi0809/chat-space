8dcccfaeb8bce9aec3a0d5538effe3232ea952d3
yuppi0809 <yuppi@YukonoMacBook-ea.local>
2019-02-13 22:22:29 +0900

    define show_messages and show_username for sidebar and chat banner

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,4 +3,20 @@ class Group < ApplicationRecord
   has_many :group_users
   has_many :users, through: :group_users
   validates :name, presence: true
+
+  def show_messages
+    if (last_message = self.messages.last).present?
+      last_message.content? ? last_message.content : "画像が送信されました"
+    else
+      "まだメッセージはありません"
+    end
+  end
+
+  def show_username
+    array = []
+    self.users.each do |user|
+      array << user.name
+    end
+    array.join(", ")
+  end
 end
