106e493a914f6737c00d8b747e60601b86d81f60
yuppi0809 <yuppi@YukonoMacBook-ea.local>
2019-02-19 23:24:14 +0900

    modify "created_at" data to display the date and time

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,5 @@
 $(function(){
+    var modifiedTime = message.time.slice(0, 10) + " " + message.time.slice(11, 16);
   $("#new_message").on("submit", function(e){
   e.preventDefault();
   var url = $("#new_message").attr("action");
