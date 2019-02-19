6bbc6d0c07be97acf5f799aee6f056b24b2cb4ba
yuppi0809 <yuppi@YukonoMacBook-ea.local>
2019-02-19 23:26:06 +0900

    define buildHTML

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,5 +1,24 @@
 $(function(){
+  var messageBody = $(".right-content__message-body")
+  function buildHTML(message){
     var modifiedTime = message.time.slice(0, 10) + " " + message.time.slice(11, 16);
+    var html = "<div class='message id=''>"
+    + "<p class='username'>"
+    + message.user_name
+    + "</p>"
+    + "<span class='date-and-time'>"
+    + modifiedTime
+    + "</span>"
+    + "<p class='text'>"
+    + (message.content ? message.content : "" )
+    + "</p>"
+    + "<div class='image'>"
+    + (message.image.url ? "<image src=\"" + message.image.url + "\">": "")
+    + "</div>"
+    + "</div>"
+    messageBody.append(html);
+  }
+
   $("#new_message").on("submit", function(e){
   e.preventDefault();
   var url = $("#new_message").attr("action");
