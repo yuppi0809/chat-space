947a964c350de61524db1d103256ef6f9e6c405c
yuppi0809 <yuppi@YukonoMacBook-ea.local>
2019-02-19 17:03:51 +0900

    add 'respond_to' to run different code depending on html request or json request

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,9 +8,12 @@ class MessagesController < ApplicationController
 
   def create
     @message = @group.messages.new(message_params)
+
     if @message.save
-      flash[:notice] = "メッセージが送信されました"
-      redirect_to group_messages_path(@group)
+      respond_to do |format|
+        format.html { flash[:notice] = "メッセージが送信されました", redirect_to group_messages_path(@group) }
+        format.json
+      end
 
     else
       @messages = @group.messages.includes(:user)
