94ba6e75084d6dcd5ac2306db628a88299a0ea9f
yuppi0809 <yuppi@YukonoMacBook-ea.local>
2019-02-20 18:54:50 +0900

    define function "appendSelectedUser"

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -21,6 +21,17 @@ $(function(){
     listDisplay.append(html);
   }
 
+  function appendSelectedUser(id, name){
+    var html =
+      `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
+        <input name='group[user_ids][]' type='hidden' value='${id}'>
+        <p class='chat-group-user__name'>${name}</p>
+        <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+      </div>`
+
+    selectedUsersDisplay.append(html);
+  }
+
   $("#user-search-field").on("keyup", function(){
     var input = $(this).val();
       $.ajax({
