09806190de76896b0c80d3a1a4d042cca6f448c5
yuppi0809 <yuppi@YukonoMacBook-ea.local>
2019-02-20 11:58:02 +0900

    change single quote to double quote and backslash

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,17 +2,17 @@ $(function(){
   var messageBody = $(".right-content__message-body")
   function buildHTML(message){
     var modifiedTime = message.time.slice(0, 10) + " " + message.time.slice(11, 16);
-    var html = "<div class='message id=''>"
-    + "<p class='username'>"
+    var html = "<div class=\"message\">"
+    + "<p class=\"username\">"
     + message.user_name
     + "</p>"
-    + "<span class='date-and-time'>"
+    + "<span class=\"date-and-time\">"
     + modifiedTime
     + "</span>"
-    + "<p class='text'>"
+    + "<p class=\"text\">"
     + (message.content ? message.content : "" )
     + "</p>"
-    + "<div class='image'>"
+    + "<div class=\"image\">"
     + (message.image.url ? "<image src=\"" + message.image.url + "\">": "")
     + "</div>"
     + "</div>"
@@ -42,7 +42,7 @@ $(function(){
     buildHTML(message);
     scrollToBottom();
     }).fail(function(){
-      alert("エラーが発生しました")
+      alert("エラーが発生しました");
     });
   });
 });
