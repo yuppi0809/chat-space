003f6beac1d6c6892a708e9af80b72a84317554b
yuppi0809 <yuppi@YukonoMacBook-ea.local>
2019-02-20 18:46:46 +0900

    define function "appendUser"

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -3,6 +3,16 @@ $(function(){
   var listDisplay = $("#user-search-result");
   var selectedUsersDisplay = $(".chat-group-form__field:nth-child(5)").find(".chat-group-form__field--right");
 
+  function appendUser(user){
+    var html =
+        `<div class="chat-group-user clearfix">
+           <p class="chat-group-user__name">${ user.name }</p>
+           <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
+        </div>`
+
+    listDisplay.append(html);
+    }
+
   $("#user-search-field").on("keyup", function(){
     var input = $(this).val();
       $.ajax({
