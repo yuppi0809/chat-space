b976f0e691b1cd5eb0cd350c97bf5b639d7cafc1
yuppi0809 <yuppi@YukonoMacBook-ea.local>
2019-02-20 18:46:01 +0900

    define variables for frequently used elements

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,5 +1,8 @@
 $(function(){
 
+  var listDisplay = $("#user-search-result");
+  var selectedUsersDisplay = $(".chat-group-form__field:nth-child(5)").find(".chat-group-form__field--right");
+
   $("#user-search-field").on("keyup", function(){
     var input = $(this).val();
       $.ajax({
