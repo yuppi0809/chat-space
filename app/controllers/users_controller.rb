341a3446458a6f4a5bc5ccd821e265607b1d8a5e
yuppi0809 <yuppi@YukonoMacBook-ea.local>
2019-02-20 19:46:07 +0900

    exclude current_user from the users list

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
   def index
-    @users = User.where('name LIKE(?)', "#{params[:search]}%")
+    @users = User.where('name LIKE(?)', "#{params[:search]}%").where.not(id: current_user.id)
   end
 
   def edit
