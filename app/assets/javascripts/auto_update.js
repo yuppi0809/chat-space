2f397c716b0c0be2f1eed38905ca9a6a3b250a7d
yuppi0809 <yuppi@YukonoMacBook-ea.local>
2019-02-21 18:39:25 +0900

    define function buildHTML
    最新のメッセージを取得し、messageBodyにアペンドするファンクション

--- app/assets/javascripts/auto_update.js
+++ app/assets/javascripts/auto_update.js
@@ -1,6 +1,25 @@
 $(function(){
   var messageBody = $(".right-content__message-body")
 
+  function buildHTML(message){
+    var modifiedTime = message.time.slice(0, 10) + " " + message.time.slice(11, 16);
+    var html = "<div class=\"message\">"
+    + "<p class=\"username\">"
+    + message.user_name
+    + "</p>"
+    + "<span class=\"date-and-time\">"
+    + modifiedTime
+    + "</span>"
+    + "<p class=\"text\">"
+    + (message.content ? message.content : "" )
+    + "</p>"
+    + "<div class=\"image\">"
+    + (message.image.url ? "<image src=\"" + message.image.url + "\">": "")
+    + "</div>"
+    + "</div>"
+    messageBody.append(html);
+  }
+
   function callAjax(){
     var message_id = $('.message:last').data('id');
     var url = $(".form-wrapper .new_message").attr("action");
