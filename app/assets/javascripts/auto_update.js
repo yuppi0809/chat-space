cc525404a80bdb6ea7b84aa1d21a26fc190ae08a
yuppi0809 <yuppi@YukonoMacBook-ea.local>
2019-02-21 18:36:34 +0900

    define function callAjax
    最新のメッセージのidをパラムスに渡す

--- app/assets/javascripts/auto_update.js
+++ app/assets/javascripts/auto_update.js
@@ -1,3 +1,16 @@
 $(function(){
   var messageBody = $(".right-content__message-body")
 
+  function callAjax(){
+    var message_id = $('.message:last').data('id');
+    var url = $(".form-wrapper .new_message").attr("action");
+
+    $.ajax({
+    type: 'GET',
+    url: url,
+    data:{
+      message:{
+      id: message_id
+      }
+    },
+    dataType: 'json'
